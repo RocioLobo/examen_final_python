e345370c5acbd83bb9184ab88337c257196ed122
RocioRosalindaLoboJacinto <lobojacintorociorosalinda#@gmail.com>
2024-07-17 11:00:26 -0500

    ejercicio:dos

--- ejercicio_dos.py
+++ ejercicio_dos.py
@@ -1,3 +1,6 @@
 """
 crear una funcion que haciendo uso del metodo filter me filtre los numeros primos de una lista pasada por parametros
 """
+impares=[4,8,2,5,7,10,6,5,3,20]
+impares=list(filter(lambda n:n%2!=0,impares))  
+print(impares)
