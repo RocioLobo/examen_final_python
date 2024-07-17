9e24417fe342b01aeb6ec42e8948f265be85d565
RocioRosalindaLoboJacinto <lobojacintorociorosalinda#@gmail.com>
2024-07-17 10:02:46 -0500

    ejercicio:cinco

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -4,3 +4,18 @@ ejm:
 entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
+
+
+nombre="jose"
+apellido="alvarez"
+programa_estudio="APSTI"
+semestre="III"
+def alumno(nombre,apellido,programa_estudio,semestre):
+    return{
+        "nombre":"jose",
+        "apellido":"alvarez",
+        "programa_estudio":"APSTI",
+        "semestre":"III"
+
+    }
+print(alumno(nombre,apellido,programa_estudio,semestre))
